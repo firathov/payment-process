da9c39335d274682f760fbe412b166e98abc99da
firathov <firat.hov@gmail.com>
2023-08-31 03:17:30 +0400

    implemented Payment controller and form

---
+++ src/Ports/Payment/Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Ports\Payment;
+
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Exception;
+use Lib\Kernel;
+use Lib\Repository\ProductRepository;
+use Ports\AbstractController;
+use Ports\Payment\Form\PaymentForm;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request, RequestStack, Response};
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @param Request $request
+ *
+ * @throws ORMException
+ * @throws OptimisticLockException
+ * @throws Exception
+ *
+ * @return JsonResponse
+ */
+#[Route(path: '/payment-process', name: 'payment_process', methods: ['GET', 'POST'])]
+class Controller extends AbstractController
+{
+    public function __construct(
+        RequestStack $requestStack,
+        Kernel $kernel,
+        private readonly iterable $paymentProcessors
+    ) {
+        parent::__construct($requestStack, $kernel);
+    }
+
+    public function __invoke(
+        Request $request,
+        ProductRepository $productRepository
+    ): Response {
+        $form = $this->createForm(PaymentForm::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && !$form->isValid()) {
+            $message = $this->getFormError($form);
+
+            return new JsonResponse(['success' => false, 'error' => $message], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = $form->getData();
+        $product = $productRepository->find($data['product']);
+
+        if (!isset($this->paymentProcessors[$data['paymentProcessor']])) {
+            return new JsonResponse(['error' => 'Invalid payment processor'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $paymentProcessor = $this->paymentProcessors[$data['paymentProcessor']];
+
+        $paymentSuccess = $paymentProcessor->processPayment($product->getPrice());
+
+        if ($paymentSuccess) {
+            return new JsonResponse(['success' => true, 'message' => 'Payment Success'], Response::HTTP_OK);
+        } else {
+            return new JsonResponse(['error' => 'Payment failed'], Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
---
+++ src/Ports/Payment/Form/PaymentForm.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Ports\Payment\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\{AbstractType, FormBuilderInterface};
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\{Length, NotBlank, Positive, Regex};
+
+class PaymentForm extends AbstractType
+{
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('product', TextType::class, [
+                'required' => true,
+                'label' => 'Product',
+                'constraints' => [
+                    new Regex('/^\d+$/', 'This value is not a valid product'),
+                    new Positive(['message' => 'product field cannot be less than 1']),
+                    new NotBlank(['message' => 'product field is empty']),
+                ],
+            ])
+            ->add('taxNumber', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'taxNumber field is empty']),
+                    new Regex([
+                        'pattern' => '/^((DE|GR)\d{9})|(IT\d{11})|(FR\w{12})$/',
+                        'message' => 'Invalid tax number format.',
+                    ]),
+                ],
+            ])
+            ->add('couponCode', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'maxMessage' => 'couponCode must not be longer than 5 characters',
+                        'max' => 5,
+                    ]),
+                ],
+            ])
+            ->add('paymentProcessor', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'paymentProcessor field is empty']),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
