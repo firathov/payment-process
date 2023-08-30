ef47d9bc8a0151a3efb97f0e756c7d4498177a52
firathov <firat.hov@gmail.com>
2023-08-31 03:14:41 +0400

    removed unsued code from AbstractController

--- src/Ports/AbstractController.php
+++ src/Ports/AbstractController.php
@@ -2,16 +2,10 @@
 
 namespace Ports;
 
-use Doctrine\Persistence\ManagerRegistry;
 use Lib\Kernel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstractController;
-use Symfony\Component\Form\{FormErrorIterator, FormInterface};
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\{Request, RequestStack};
-use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Messenger\Stamp\HandledStamp;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 abstract class AbstractController extends SymfonyAbstractController
 {
@@ -20,43 +14,8 @@ abstract class AbstractController extends SymfonyAbstractController
     public function __construct(
         RequestStack $requestStack,
         protected readonly Kernel $kernel,
-        protected readonly ManagerRegistry $doctrine,
-        protected readonly MessageBusInterface $bus,
-        protected readonly SerializerInterface $serializer,
-        protected readonly DenormalizerInterface $denormalizer,
-        protected readonly ValidatorInterface $validator
     ) {
         $this->request = $requestStack->getCurrentRequest();
-    }
-
-    protected function handle(object $query)
-    {
-        $envelope = $this->bus->dispatch($query);
-        $handledStamp = $envelope->last(HandledStamp::class);
-
-        return $handledStamp->getResult();
-    }
-
-    /**
-     * @param string $message
-     */
-    protected function flashSuccess(string $message): void
-    {
-        $this->addFlash('success', $message);
-    }
-
-    /**
-     * @param FormErrorIterator|string $data
-     */
-    protected function flashError($data): void
-    {
-        if ($data instanceof FormErrorIterator) {
-            foreach ($data as $message) {
-                $this->addFlash('error', $message->getMessage());
-            }
-        } else {
-            $this->addFlash('error', $data);
-        }
     }
 
     /**
